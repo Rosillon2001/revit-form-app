833a527268afd90a85410bf406c127ecccb7a0ce
drosillon <rosillon2001@gmail.com>
2024-06-13 18:27:22 -0400

    Update db-service.service.ts

--- src/app/shared/services/mock-db/db-service.service.ts
+++ src/app/shared/services/mock-db/db-service.service.ts
@@ -45,7 +45,7 @@ export class DbServiceService implements InMemoryDbService{
         'in_car_checks': {'warning_lights': 'checked'},
         'notes': 'Some notes', 'operational': true, 'requires_maintenance': false
       },
-      {'id': 5, 'name': 'John Doe', 'org_name': 'Car Speed', 'image': '../../../assets/images/mustang.webp', 'car_group': 'Group A', 'car_brand': 'FORD', 
+      {'id': 5, 'name': 'Freddie Mercury', 'org_name': 'Car Speed', 'image': '../../../assets/images/mustang.webp', 'car_group': 'Group A', 'car_brand': 'FORD', 
         'car_model': '50th Aniversary Mustang', 'car_year': '2015', 'car_color': 'Blue', 'trip': 'post_trip',
         'odometer': '63787', 'address': '1234 Main St.', 
         'exterior_checks': { 'tires': 'checked', 'spare_wheel': 'checked', 'lights': 'checked', 'physical_damage': 'checked', 'leaking_fluids': 'checked'}, 
